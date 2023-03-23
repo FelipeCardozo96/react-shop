abcf18f3803f585e7a627d1cc48f1215d7c5eb99
FelipeCardozo96 <felipemcr996@gmail.com>
2023-03-22 21:41:10 -0300

    se agregaron todos los componentes, las paginas y los containers correspondientes del proyecto

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from '../pages/Login';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
-import CreateAccount from '../containers/CreateAccount';
+import PasswordRecovery from '../pages/PasswordRecovery';
+import EmailSent from '../pages/EmailSent';
+import CreateAccount from '../pages/CreateAccount';
+import MyAccount from '../pages/MyAccount';
+import AccountMenu from '../containers/AccountMenu';
+import MobileMenu from '../containers/MobileMenu';
+import MyOrder from '../pages/MyOrder';
+import OrderHistory from '../pages/OrderHistory';
+import ProductDescription from '../components/ProductDescription';
+import Chart from '../pages/Chart';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
@@ -16,8 +24,16 @@ const App = () => {
 			<Switch>
 				<Route exact path="/" component={Home}/>
 				<Route exact path="/login" component={Login}/>
-				<Route exact path ="/recovery-password" component={RecoveryPassword} />
+				<Route exact path="/password-recovery" component={PasswordRecovery}/>
+				<Route exact path ="/emailsent" component={EmailSent} />
 				<Route exact path='/create-account' component={CreateAccount}/>
+				<Route exact path='/my-account' component={MyAccount}/>
+				<Route exact path='/account' component={AccountMenu}/>
+				<Route exact path='/mobilemenu' component={MobileMenu}/>
+				<Route exact path='/myorder' component={MyOrder}/>
+				<Route exact path='/orderhistory' component={OrderHistory}/>
+				<Route exact path='/prd' component={ProductDescription}/>
+				<Route exact path='/chart' component={Chart}/>
 				<Route component={NotFound}/>
 			</Switch>
 		</Layout>
